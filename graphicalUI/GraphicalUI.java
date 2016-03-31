4e5bffa4aaec37c7670a69ec827107636c5392b1
Svadilfare <robert.dighem@gmail.com>
2016-03-31 16:41:19 +0200

    andra committen, kompletteringar i GUI

--- graphicalUI/GraphicalUI.java
+++ graphicalUI/GraphicalUI.java
@@ -9,16 +9,53 @@ public class GraphicalUI extends JFrame {
     private ValuableList myValuableList;
 
     public GraphicalUI(){
-        setLayout(new FlowLayout());
-
-
-        JLabel label = new JLabel("Awesome fält"); 
-        add(label);
+        super("Register");
+        setLayout(new BorderLayout());
+//        JPanel north = new JPanel();
+        JLabel labelValuables = new JLabel("Valuables", SwingConstants.CENTER);
+        add(labelValuables, BorderLayout.NORTH);
 
         JTextField field = new JTextField(10);
-        add(field);
+        add(field, BorderLayout.CENTER);
+//        field.setEditable(false);
+        JScrollPane scroll = new JScrollPane(field);
+        add(scroll);
+
+//        kolla storleken på fältet, lägg in scrollfunktion
+
+        JPanel east = new JPanel();
+        JLabel labelSort = new JLabel();
+//        int height = getHeight()/2;
+        east.add(Box.createRigidArea(new Dimension(0, 325)));
+        east.add(new JLabel("Sort by"));
+        JRadioButton name = new JRadioButton("Name");
+        east.add(name);
+        JRadioButton value = new JRadioButton("Value");
+        east.add(value);
+//        east.add(new JRadioButton("Name"));
+//        east.add(new JRadioButton("Value"));
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(name);
+        bg.add(value);
+
+//        gruppera radiobuttons
+        east.setLayout(new BoxLayout(east, BoxLayout.Y_AXIS));
+//        east.setLayout(new GridLayout(3,1));
+        add(east, BorderLayout.EAST);
+
+
+
+        JPanel south = new JPanel();
+        String[] choice = {"", "Jewellry", "Stock", "Apparatus"};
+        JComboBox<String> comboChoice = new JComboBox<String>(choice);
+        south.add(new JLabel("New:"));
+        south.add(comboChoice);
+        south.add(new JButton("Show"));
+        south.add(new JButton("Stock market crash"));
+        add(south, BorderLayout.SOUTH);
+
         setVisible(true);
-        setSize(300, 100);
+        setSize(900, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
